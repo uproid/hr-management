b2988b983296ab350f56d49c6804678a0522cf60
Farhad Ziaee <fardzia@gmail.com>
2021-10-11 23:13:55 +0200

    POST employee endpoint

---
+++ app/Http/Controllers/ToombaApi/DependentController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\ToombaApi;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class DependentController extends ApiController
+{
+    //
+}
